db8044f3f5c82d9a8da94a42f3860ad69ad761c6
mw138 <mattheww138@gmail.com>
2024-02-29 16:38:48 +0000

    fix: apostrophy format fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,7 @@ export default function Home() {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">
-              Hi, my name is Matthew Watson
-            </h1>
+            <h1 className="text-5xl font-bold">Hi, I&apos;m Matthew Watson</h1>
             <p className="py-6">
               I am a programmer from the United Kingdom with 7+ years of
               self-taught and College tuition using Unity and C#.
