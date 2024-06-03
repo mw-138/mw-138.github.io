bf155f4f7809e2eb1745d079f61aa43c13f76b75
mw138 <mattheww138@gmail.com>
2024-06-03 22:52:12 +0100

    feat: improvements to navbar responsiveness

--- app/websites/premier_league_redesign/components/Navbar.tsx
+++ app/websites/premier_league_redesign/components/Navbar.tsx
@@ -8,10 +8,44 @@ type NavbarProps = {
   teams: Team[];
 };
 
-const Navbar = ({ teams }: NavbarProps) => {
+const SmallNavbar = ({ teams }: NavbarProps) => {
   return (
-    <>
-      <div className="flex flex-row items-center justify-between bg-white p-4 md:hidden">
+    <div className="flex flex-row items-center justify-between bg-white p-4 md:hidden">
+      <Image
+        src="/premier-league.png"
+        width={96}
+        height={96}
+        alt=""
+        className="aspect-square"
+      />
+      <div className="rounded p-2 text-premier-league-purple transition-colors hover:bg-premier-league-purple hover:text-white">
+        <IoMdMenu size={46} />
+      </div>
+    </div>
+  );
+};
+
+const MediumNavbar = ({ teams }: NavbarProps) => {
+  return (
+    <div className="hidden flex-row items-center justify-between bg-white p-4 max-lg:flex max-md:hidden">
+      <Image
+        src="/premier-league.png"
+        width={96}
+        height={96}
+        alt=""
+        className="aspect-square"
+      />
+      <div className="rounded p-2 text-premier-league-purple transition-colors hover:bg-premier-league-purple hover:text-white">
+        <IoMdMenu size={46} />
+      </div>
+    </div>
+  );
+};
+
+const LargeNavbar = ({ teams }: NavbarProps) => {
+  return (
+    <div className="hidden flex-row border-y-8 border-white bg-white md:flex lg:border-t-0">
+      <div className="flex items-center justify-center p-2">
         <Image
           src="/premier-league.png"
           width={96}
@@ -19,78 +53,74 @@ const Navbar = ({ teams }: NavbarProps) => {
           alt=""
           className="aspect-square"
         />
-        <div className="rounded p-2 text-premier-league-purple transition-colors hover:bg-premier-league-purple hover:text-white">
-          <IoMdMenu size={46} />
-        </div>
       </div>
-      <div className="hidden flex-row border-y-8 border-white bg-white md:flex lg:border-t-0">
-        <div className="flex items-center justify-center p-2">
-          <Image
-            src="/premier-league.png"
-            width={96}
-            height={96}
-            alt=""
-            className="aspect-square"
-          />
+      <div className="flex flex-1 flex-col">
+        <div className="hidden flex-row gap-4 p-2 lg:flex">
+          {teams.map((team, index) => (
+            <Image
+              key={index}
+              src={team.image}
+              width={32}
+              height={32}
+              alt=""
+              className="aspect-square transition-transform hover:scale-110"
+            />
+          ))}
         </div>
-        <div className="flex flex-1 flex-col">
-          <div className="hidden flex-row gap-4 p-2 lg:flex">
-            {teams.map((team, index) => (
-              <Image
-                key={index}
-                src={team.image}
-                width={32}
-                height={32}
-                alt=""
-                className="aspect-square transition-transform hover:scale-110"
-              />
-            ))}
-          </div>
-          <nav className="flex items-center rounded-tl-lg bg-premier-league-purple p-2">
-            <ul className="flex flex-row gap-2">
-              {[
-                "Premier League",
-                "Fantasy",
-                "Football & Community",
-                "About",
-                "More",
-              ].map((tab, index) => (
-                <li
-                  key={index}
-                  className="text-md flex cursor-pointer flex-row items-center gap-2 rounded p-2 font-bold text-white transition-colors hover:bg-premier-league-purple"
-                >
-                  {tab}
-                  <FaChevronDown size={10} />
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <div className="flex flex-row items-center overflow-hidden rounded-bl-lg bg-premier-league-purple">
+        <nav className="flex items-center rounded-tl-lg bg-premier-league-purple p-2">
+          <ul className="flex flex-row gap-2">
             {[
-              "Home",
-              "Fixtures",
-              "Results",
-              "Tables",
-              "Transfers",
-              "Stats",
-              "News",
-              "Video",
-              "Watch Live",
-              "Tickets",
-              "Clubs",
-              "Players",
-              "Awards",
+              "Premier League",
+              "Fantasy",
+              "Football & Community",
+              "About",
+              "More",
             ].map((tab, index) => (
-              <button
+              <li
                 key={index}
-                className="rounded-tl-lg rounded-tr-lg p-4 text-xs font-bold text-white transition-colors first:rounded-tl-none hover:bg-premier-league-pink"
+                className="text-md flex cursor-pointer flex-row items-center gap-2 rounded p-2 font-bold text-white transition-colors hover:bg-premier-league-purple"
               >
                 {tab}
-              </button>
+                <FaChevronDown size={10} />
+              </li>
             ))}
-          </div>
+          </ul>
+        </nav>
+        <div className="flex flex-row items-center overflow-hidden rounded-bl-lg bg-premier-league-purple">
+          {[
+            "Home",
+            "Fixtures",
+            "Results",
+            "Tables",
+            "Transfers",
+            "Stats",
+            "News",
+            "Video",
+            "Watch Live",
+            "Tickets",
+            "Clubs",
+            "Players",
+            "Awards",
+          ].map((tab, index) => (
+            <button
+              key={index}
+              className="rounded-tl-lg rounded-tr-lg p-4 text-xs font-bold text-white transition-colors first:rounded-tl-none hover:bg-premier-league-pink"
+            >
+              {tab}
+            </button>
+          ))}
         </div>
       </div>
+    </div>
+  );
+};
+
+const Navbar = ({ teams }: NavbarProps) => {
+  return (
+    <>
+      <SmallNavbar teams={teams} />
+      {/* <MediumNavbar teams={teams} /> */}
+      <LargeNavbar teams={teams} />
     </>
   );
 };
