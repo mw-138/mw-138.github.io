907a8bbec44d8c498db42441d13c7c00f4ecb1c9
mw138 <mattheww138@gmail.com>
2024-06-03 22:51:54 +0100

    feat: code refactor

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Projects } from "@/data/Projects";
+import { Projects } from "@/data/projects";
 import { notFound } from "next/navigation";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
