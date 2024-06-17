81bc47c5f92799f39f2aa59b9226ada51267bbf9
mw138 <mattheww138@gmail.com>
2024-06-17 23:29:16 +0100

    feat: moved subscription edit buttons
    Moved the action buttons when editing a subscription to the top of the form.

--- app/websites/subscription_tracker/components/SubscriptionForm.tsx
+++ app/websites/subscription_tracker/components/SubscriptionForm.tsx
@@ -19,9 +19,36 @@ export default function SubscriptionForm() {
   } = useSubscriptionTrackerContext();
   return (
     <div className="flex-1 p-4 text-white backdrop-blur-md">
-      <h1 className="pb-2 font-bold uppercase">
-        {editingSubscription ? "Edit" : "Add"} Subscription
-      </h1>
+      <div className="mb-2 flex h-10 items-center justify-between py-2">
+        <h1 className="font-bold uppercase">
+          {editingSubscription ? "Edit" : "Add"} Subscription
+        </h1>
+        {editingSubscription && (
+          <div className="flex flex-row gap-4">
+            <button
+              className="flex flex-1 flex-row items-center justify-center gap-2 rounded-md bg-white p-2 px-4 py-2 text-slate-700 transition-colors hover:bg-green-500 hover:text-white active:bg-green-600 disabled:bg-red-900 disabled:text-red-600"
+              onClick={handleFormSubmit}
+            >
+              <MdSave />
+              Save
+            </button>
+            <button
+              className="flex flex-1 flex-row items-center justify-center gap-2 rounded-md bg-white p-2 px-4 py-2 text-slate-700 transition-colors hover:bg-red-500 hover:text-white active:bg-red-600 disabled:bg-red-900 disabled:text-red-600"
+              onClick={() => deleteSubscription(selectedSubscriptionIndex)}
+            >
+              <MdDelete />
+              Delete
+            </button>
+            <button
+              className="flex flex-1 flex-row items-center justify-center gap-2 rounded-md bg-white p-2 px-4 py-2 text-slate-700 transition-colors hover:bg-slate-500 hover:text-white active:bg-slate-600 disabled:bg-red-900 disabled:text-red-600"
+              onClick={cancelEdit}
+            >
+              <MdCancel />
+              Cancel
+            </button>
+          </div>
+        )}
+      </div>
       <form className="flex flex-col gap-4" onSubmit={handleFormSubmit}>
         <div className="flex flex-col gap-1">
           <label htmlFor="label">Label</label>
@@ -76,30 +103,7 @@ export default function SubscriptionForm() {
         </div>
         {editingSubscription ? (
           <div className="flex flex-col gap-4">
-            <div className="flex flex-row gap-4">
-              <button
-                className="flex flex-1 flex-row items-center justify-center gap-2 rounded-md bg-white p-2 text-slate-700 transition-colors hover:bg-green-500 hover:text-white active:bg-green-600 disabled:bg-red-900 disabled:text-red-600"
-                onClick={handleFormSubmit}
-              >
-                <MdSave />
-                Save
-              </button>
-              <button
-                className="flex flex-1 flex-row items-center justify-center gap-2 rounded-md bg-white p-2 text-slate-700 transition-colors hover:bg-red-500 hover:text-white active:bg-red-600 disabled:bg-red-900 disabled:text-red-600"
-                onClick={() => deleteSubscription(selectedSubscriptionIndex)}
-              >
-                <MdDelete />
-                Delete
-              </button>
-              <button
-                className="flex flex-1 flex-row items-center justify-center gap-2 rounded-md bg-white p-2 text-slate-700 transition-colors hover:bg-slate-500 hover:text-white active:bg-slate-600 disabled:bg-red-900 disabled:text-red-600"
-                onClick={cancelEdit}
-              >
-                <MdCancel />
-                Cancel
-              </button>
-            </div>
-            <div className="rounded-md bg-slate-700 p-2">
+            <div className="rounded-md bg-white/20 p-2">
               <h1 className="font-bold uppercase">Stats</h1>
               <div
                 className="tooltip cursor-help"
