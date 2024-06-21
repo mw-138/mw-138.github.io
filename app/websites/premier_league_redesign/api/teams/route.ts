98a44a6b6e06fb3d2ceee51066af0d14d0d150af
mw138 <mattheww138@gmail.com>
2024-06-21 21:22:23 +0100

    Delete route.ts

--- app/websites/premier_league_redesign/api/teams/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(): Promise<NextResponse> {
-  return NextResponse.json({ teams: ["one", "two"] });
-}
-
-export async function POST(request: NextRequest): Promise<NextResponse> {
-  return NextResponse.json({ teams: ["one", "two"] });
-}
